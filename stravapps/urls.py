da444f2d3c430f0276474043fa0159938f4c15c8
ggila <ggilaber@student.42.fr>
2016-09-11 10:01:49 +0200

    stravapp renamed to stravapps

--- stravapps/urls.py
+++ stravapps/urls.py
@@ -1,4 +1,4 @@
-"""stravapp URL Configuration
+"""stravapps URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^polls/', include('dispact.urls')),
+    url(r'^dispact/', include('dispact.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- stravapps/wsgi.py
+++ stravapps/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for stravapp project.
+WSGI config for stravapps project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stravapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stravapps.settings")
 
 application = get_wsgi_application()
