aeb3f543279ce1de963e874ab76234ade6130ab3
ggila <ggilaber@student.42.fr>
2016-09-11 12:00:28 +0200

    add activity and track model

--- dispact/models.py
+++ dispact/models.py
@@ -1,5 +1,15 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Activity(models.Model):
+    sport = models.CharField(max_length=30)
+    date = models.DateTimeField('date')
+    duration = models.DurationField('elapsed time')
+    distance = models.FloatField('distance')
+
+class TrkPt(models.Model):
+    act = models.ForeignKey(Activity, on_delete=models.CASCADE)
+    delta = models.DurationField('elapsed time')
+    lon = models.FloatField('longitude')
+    lat = models.FloatField('latitude')
+    ele = models.FloatField('elevation')
+    hr = models.PositiveSmallIntegerField('heart rate')   # might be not set
